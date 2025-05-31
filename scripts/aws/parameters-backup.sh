f2222032aabbae5dd57895605fa1f5922436dfb3
Leonardo Flores <Leonardo.Flores@popular.com>
2025-05-30 21:52:56 -0300

    feat: adding first version of parameters backup

---
+++ scripts/aws/parameters-backup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# =======================================================
+# import utils code
+# Read <root dir>/tools/utils.sh for more info
+SCRIPT_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
+BASE_DIR="$(realpath "$SCRIPT_DIR/../../")"
+source "${BASE_DIR}/tools/utils.sh"
+# =======================================================
+
+get_parameter_info() {
+    FILENAME="$1"
+    echo -e "Backing up: $1"
+    VALUE=$(aws ssm get-parameter \
+        --name " $1" \
+        --query "Parameter.Value" \
+        --output text \
+    )
+    # aws ssm get-parameter --name "/bppr/mb/move-money/popular-pay/certificate" --query "Parameter.Value" --output text
+    # aws ssm get-parameter --name " /bppr/mb40/accounts/il/connection_info" --query "Parameter.Value" --output text
+    # aws ssm get-parameters-by-path --path "/bppr/mb/move-money/popular-pay/certificate"
+    # aws ssm describe-parameters --query "Parameters[*].Name" --output text
+    # aws ssm describe-parameters --query "Parameters[?contains(Name,'config')].Name" --output string
+    # aws ssm describe-parameters --query "Parameters[?contains(Name,'/bppr/mb40/accounts/il/connection_info')].Name" --output text
+
+    SAFE_NAME=$(echo $FILENAME | sed -E 's|^(/[^/]+)(.*)|\1\2|; s|/|_|g; s|^(/[^/]+)_|\1/|')
+    echo "$VALUE" > "$2/${SAFE_NAME}.json"
+    return
+}
+
+# First I check if current folder is git repo
+folder="./"
+PARAMETERS_FILE=".parameters"
+if [[ ! -f "$PARAMETERS_FILE" ]]; then
+     echo -e "${YELLOW} - ⚠ The file $PARAMETERS_FILE is required to know which parameters you needs backup${NC}"    
+     exit 1
+fi
+
+counter=0;
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output json)
+ACCOUNT_ID=$(echo "$ACCOUNT_ID" | tr -d '"[:space:]')
+
+if [[ ! -f $ACCOUNT_ID ]]; then
+    echo -e "Creando ${ACCOUNT_ID}"
+    mkdir -p "$ACCOUNT_ID"
+fi
+
+while IFS= read -r parameter; do
+    get_parameter_info $parameter $ACCOUNT_ID
+    ((counter++))
+done < "$PARAMETERS_FILE"
+
+echo -e "Checked ${counter} parameters"
